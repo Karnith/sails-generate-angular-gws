e0e751eedd3bc7c17f094d092c6acc91ecb5b262
Matthew Marino <mmarino@jcmar.com>
2014-10-15 14:44:19 -0700

    refactor web server config to file

--- templates/tasks/config/ngWebserver.js
+++ templates/tasks/config/ngWebserver.js
@@ -10,21 +10,7 @@
 module.exports = function(gulp, plugins, growl) {
 	gulp.task('ngWebserver', function() {
 		return gulp.src('app')
-		    .pipe(plugins.webserver({
-                host: 'localhost',
-                port: 80,
-                livereload: true,
-                directoryListing: false,
-                fallback: 'index.html',
-                open: false,
-                https: false,
-                middleware: [],
-                proxies: [{
-                    source: '/api',
-                    target: 'localhost:1337/api'
-                }]
-
-            }))
+		    .pipe(plugins.webserver(require('../../config/ngwebserver').ngwebserver))
 			.pipe(plugins.if(growl, plugins.notify({ message: 'Angular web server started' })));
 	});
 };
